0fd615fcb0ba44b90f722e42c1b1d19bf8a3c43c
Virendra Shinde <91678077+virendras113@users.noreply.github.com>
2023-05-05 22:30:04 +0530

    Add files via upload

---
+++ fall_detection.py
@@ -0,0 +1,66 @@
+import cv2
+import time
+import image_email_fall
+
+
+fitToEllipse = False
+cap = cv2.VideoCapture('fallvid3.mp4')
+time.sleep(2)
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+j = 0
+count = 0
+while(1):
+    ret, frame = cap.read()
+    
+    #Convert each frame to gray scale and subtract the background
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        fgmask = fgbg.apply(gray)
+        
+        #Find contours
+        contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if contours:
+        
+            # List to hold all areas
+            areas = []
+
+            for contour in contours:
+                ar = cv2.contourArea(contour)
+                areas.append(ar)
+            
+            max_area = max(areas, default = 0)
+
+            max_area_index = areas.index(max_area)
+
+            cnt = contours[max_area_index]
+
+            M = cv2.moments(cnt)
+            
+            x, y, w, h = cv2.boundingRect(cnt)
+
+            cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 3, maxLevel = 0)
+            
+            if h < w:
+                j += 1
+                
+            if j > 10:
+                print("FALL", count)
+                count+=1
+                cv2.putText(fgmask, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                image_email_fall.SendMail()
+
+            if h > w:
+                j = 0 
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+            cv2.imshow('video', frame)
+        
+            if cv2.waitKey(33) == 27:
+             break
+    except Exception as e:
+        break
+cv2.destroyAllWindows()
